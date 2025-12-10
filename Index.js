11841b3f4b25319ed38271b8860cdc60d8e03536
Swati <swati.kumari12381@gmail.com>
2025-12-10 14:15:23 +0530

    first commit

---
+++ Index.js
@@ -0,0 +1,4 @@
+console.log("My name is swati Kumari.","I have a 5+ years of experience,")
+console.log("including 3 years as a Ruby on Rails developer")
+console.log("and 2 years as an SQL developer.")
+console.log("I started my career at a startup company, and then I joined TCS in 2021.")
