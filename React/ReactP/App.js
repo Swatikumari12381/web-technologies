b9dc4761adef715ff747f4bb2dce2ca63303c5fe
Swati <swati.kumari12381@gmail.com>
2025-12-18 13:19:05 +0530

    feature jsx added

--- React/ReactP/App.js
+++ React/ReactP/App.js
@@ -1,22 +1,26 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-const conatiner = React.createElement(
-            ("div"),{},
-            React.createElement(("h1"), {}, " Hello React....", React.createElement("br")),
-            React.createElement(("p"), {}, "This is a paragraph rendered using React CDN." ),
-            React.createElement(("a"),
-                {
-                    href: "https://react.dev",
-                    target: "_blank"
-                },"Learn React Official Site", React.createElement("br")
-            ),
-            React.createElement(("img"),
-                {
-                    src: "https://reactjs.org/logo-og.png",
-                    alt: "React Logo",
-                    width: "400"
-                }
-            )
-        );
-        const root =ReactDOM.createRoot(document.getElementById("root"));
-        root.render(conatiner);
+
+const container = (
+      <div>
+        <h1>
+          Hello React....
+          <br />
+        </h1>
+
+        <p>This is a paragraph rendered using React CDN.</p>
+
+        <a href="https://react.dev" target="_blank">
+          Learn React Official Site
+          <br />
+        </a>
+
+        <img
+          src="https://reactjs.org/logo-og.png"
+          alt="React Logo"
+          width="400"
+        />
+      </div>
+);
+const root =ReactDOM.createRoot(document.getElementById("root"));
+root.render(container);
