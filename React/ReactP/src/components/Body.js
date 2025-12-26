f5c80af1c567583d4d9d0a5128af04292e7e7d5a
Swati <swati.kumari12381@gmail.com>
2025-12-26 11:57:20 +0530

    added feature react hooks

--- React/ReactP/src/components/Body.js
+++ React/ReactP/src/components/Body.js
@@ -1,14 +1,21 @@
+import { useState } from "react";
 import { RES_DATA } from "../utility/constants";
 import RestoCard from "./RestoCard";
 const Body = () => {
+  const [listofRes, setlistofRes] = useState(RES_DATA);
   return(
     <div>
       <div className="search">
         <input type ="text"></input>
-        <button type ="search">Submit</button>
+        <button type ="submit">Search</button>
+        <button onClick={()=>{
+          let filterData=listofRes.filter((res)=>
+          res.card.card.info.avgRating >= 4.5)
+          setlistofRes(filterData);
+        }}>Top rated</button>
       </div>
       <div className="restcard">
-        {RES_DATA.map((rest) =>
+        {listofRes.map((rest) =>
         (<RestoCard 
           key={rest.card.card.info.id}
           restData={rest.card.card}>
