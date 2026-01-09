0e139ff45f0f40b4ef39bcba419af2eacbd162e6
Swati <swati.kumari12381@gmail.com>
2026-01-09 14:45:32 +0530

    added feature search bar

--- React/ReactP/src/components/Body.js
+++ React/ReactP/src/components/Body.js
@@ -4,36 +4,44 @@ import RestoCard from "./RestoCard";
 
 const Body = () => {
   const [listofRes, setlistofRes] = useState([]);
+  const [filteredRes, setFilteredRes] =useState([])
+  const [searchText, setSearchText] = useState("")
 
   useEffect(() => {
     fetchdata();
   },[])
 
   const fetchdata = async() =>{
-    let response= await fetch("https://www.swiggy.com/dapi/restaurants/list/v5?lat=12.97530&lng=77.59100&collection=80475&tags=&sortBy=&filters=&type=rcv2&offset=0&page_type=null");
+    let response= await fetch("https://www.swiggy.com/dapi/restaurants/search/v3?lat=12.97530&lng=77.59100&str=Egg&trackingId=undefined&submitAction=ENTER&queryUniqueId=ee0e16b8-9df7-9d1a-519c-0d15d34dd297&selectedPLTab=RESTAURANT");
     let result= await response.json();
-    let extractedData = result.data.cards;
-    let exData =extractedData.filter((filterData,index)=> {
-      if(index > 1) {
-        return filterData.card.card;
-      }
-    })
-    console.log(exData);
-    setlistofRes(exData);
+    let extractedData = result.data.cards[0].groupedCard.cardGroupMap.RESTAURANT.cards;
+    console.log(extractedData);
+    setlistofRes(extractedData);
+    setFilteredRes(extractedData)
   }
+  
   return(
     <div>
       <div className="search">
-        <input type ="text"></input>
-        <button type ="submit">Search</button>
+        <input type ="text" onChange={(e)=>{
+          setSearchText(e.target.value);
+        }}></input>
+        <button onClick={()=>{
+          let finaltext =filteredRes.filter((filtertext)=>
+            filtertext.card.card.info.name.toLocaleLowerCase().includes(searchText.toLocaleLowerCase())
+        );
+          setFilteredRes(finaltext);
+        }}
+        >
+        Search</button>
         <button onClick={()=>{
           let filterData=listofRes.filter((res)=>
           res.card.card.info.avgRating >= 4.5)
-          setlistofRes(filterData);
+          setFilteredRes(filterData);
         }}>Top rated</button>
       </div>
       <div className="restcard">
-        {listofRes.map((rest, index) =>
+        {filteredRes.map((rest, index) =>
         (<RestoCard 
           key={index}
           restData={rest}>
--- React/ReactP/src/components/Header.js
+++ React/ReactP/src/components/Header.js
@@ -1,5 +1,7 @@
+import { useState } from "react";
 
 const Header = () => {
+  const [button,setButton] = useState("login")
   return(
     <div className="heading">
       <img src = "https://img.freepik.com/premium-vector/food-delivery-online-concept-vector_1162942-1594.jpg?semt=ais_hybrid&w=740&q=80" alt ="trill" width={80}></img>
@@ -8,6 +10,11 @@ const Header = () => {
           <li>Home</li>
           <li>Contact</li>
           <li>About</li>
+          <li>
+            <button className="button" onClick={()=>{
+              setButton(button === "login" ? "logout" : "login");
+            }}>{button}</button>
+          </li>
         </ul>
       </div>
     </div>
